642d79df3ff6a301e1578004bd8b04ab4510559d
Oleg V <oleg.vintonuk@gmail.com>
2019-12-30 00:22:43 +0200

    add sorting data app

---
+++ Sorting-data/precache-manifest.0dd180611959ed07c20399a9320ff277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465c12ae7c093548819fbc577b124512",
+    "url": "/React.js-solutions/Sorting-data/index.html"
+  },
+  {
+    "revision": "a2cc99ad5a493a492c42",
+    "url": "/React.js-solutions/Sorting-data/static/css/main.9a540125.chunk.css"
+  },
+  {
+    "revision": "2f278e9ddaa125c6d33f",
+    "url": "/React.js-solutions/Sorting-data/static/js/2.844f7af3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React.js-solutions/Sorting-data/static/js/2.844f7af3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2cc99ad5a493a492c42",
+    "url": "/React.js-solutions/Sorting-data/static/js/main.c778acf4.chunk.js"
+  },
+  {
+    "revision": "28868c89a9f332b853c1",
+    "url": "/React.js-solutions/Sorting-data/static/js/runtime-main.a9df3a64.js"
+  }
+]);
